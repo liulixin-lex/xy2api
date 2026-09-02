64c4b979d046cee7275c346f99ea4c9575e813be
lex_liu <lex.liu.1@outlook.com>
2026-09-02 17:03:06 +0800

    sync: normalize xy2api module paths for sub2api v0.2.0

--- backend/internal/service/openai_reasoning_effort_policy.go
+++ backend/internal/service/openai_reasoning_effort_policy.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/Wei-Shaw/sub2api/internal/domain"
+	"github.com/liulixin-lex/xy2api/internal/domain"
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
 )
